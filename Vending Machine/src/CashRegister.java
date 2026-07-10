63f7626d05fb6fce987413144534115fc08d6755
skellystronk <paradosamuel759@gmail.com>
2026-07-11 02:46:41 +0800

    Denomination and CashRegister Class logic implemented using arrays

--- Vending Machine/src/CashRegister.java
+++ Vending Machine/src/CashRegister.java
@@ -1,21 +1,91 @@
-import java.util.ArrayList;
-
 public class CashRegister {
-    ArrayList<Denomination> denominations = new ArrayList<>();
+    private int[] quantity;     //how much of the denomination/bills are held
 
     public CashRegister() {
-        InitializeDenominations();
+        this.quantity = new int[Denomination.getValidValues().length];
     }
 
-    public void InitializeDenominations()
-    {
-        this.denominations.add(new Denomination("1000", 1000));
-        this.denominations.add(new Denomination("500", 500));
-        this.denominations.add(new Denomination("100", 100));
-        this.denominations.add(new Denomination("50", 50));
-        this.denominations.add(new Denomination("20", 20));
-        this.denominations.add(new Denomination("10", 10));
-        this.denominations.add(new Denomination("5", 5));
-        this.denominations.add(new Denomination("1", 1));
+    //retuns index of denomination entered
+    private int findDenominationIndex(int value){
+        int[] validValues = Denomination.getValidValues();
+        int i;
+
+        //loops through denominations to find the index of denomination given
+        for (i = 0; i < validValues.length; i++){
+            if (validValues[i] == value){
+                return i;   //index of the denomination
+            }
+        }
+        return -1;          //invalid denomination
+    }
+
+    //adds incoming currency to inventory count
+    public void addCash(int value, int count){
+        int index = findDenominationIndex(value);
+
+        if (index != -1){
+            this.quantity[index] += count;      //increments the count fo denomination in that index
+        }
+        else{
+            System.out.println("Not a valid denomination!");
+        }
+    }
+
+    public int[] calculateChange(int amountRequired){
+        int i;
+        int[] validValues = Denomination.getValidValues();
+        int[] changeToGive = new int[validValues.length];   //array to track how many of each denomination to output
+        int remainingAmount = amountRequired;
+
+        //loop through all denominations
+        for (i = 0; i < validValues.length; i++){
+            int currentDenominationValue = validValues[i];
+
+            //if statemtnt to check if denomination will fit into remaining change
+            if (remainingAmount >= currentDenominationValue){
+                int neededDenomination = remainingAmount / currentDenominationValue;    //int to track how  many of this denomination is needed
+                int actualChangeToGive = Math.min(neededDenomination, this.quantity[i]); //compare how many we need vs how many do we have
+
+                changeToGive[i] = actualChangeToGive;
+                remainingAmount -= actualChangeToGive * currentDenominationValue;
+            }
+        }
+
+        //check to see if target change is zero/change is dispensable
+        if (remainingAmount == 0){
+            return changeToGive;    //return what denominations to give
+        }
+        else{
+            return null;            //change inside is not enough (i think conditional statement in VM.java to print out error message if(variable == null) ts)
+        }
+    }
+
+    //deducts the denominations and logs the change to give
+    public void deductCash(int[] change){
+        int[] validValues = Denomination.getValidValues();
+        int i;
+
+        for (i = 0; i < quantity.length; i++){
+            //updates actual changes to inventory
+            this.quantity[i] -= change[i];
+
+            //if successful print out a log
+            if (change[i] > 0){
+                System.out.println("Dispensing: " + change[i] + "x PHP " + validValues[i]);
+            }
+        }
+    }
+
+    //retuns the total sum of money currently inside the VM
+    public int getTotalValue(){
+        int total = 0;
+        int i;
+        int[] validValues = Denomination.getValidValues();
+
+        //loop to multiply denomination by the count of denomination
+        for (int i = 0; i < validValues.length; i++){
+            total += validValues[i] * quantity[i];
+        }
+        return total;
     }
 }
--- Vending Machine/src/Denomination.java
+++ Vending Machine/src/Denomination.java
@@ -2,17 +2,44 @@ public class Denomination {
     private final String denominationName;
     private int amount;
     private final int value;
+    private static final int[] VALID_VALUES = {1000, 500, 200, 100, 50, 20, 10, 5, 1};  //valid bills
 
+    public Denomination(int value) {
+        if (!isValid(value)) {
+            throw new IllegalArgumentException("Denomination value is invalid" + value);
+        }
 
-    public Denomination(String denominationName, int value) {
-        this.denominationName = denominationName;
         this.value = value;
     }
 
     public void setAmount(int amount) {
         this.amount = amount;
     }
-    public int getAmount() { return this.amount; }
-    public String getDenominationName() { return this.denominationName; }
-    public int getValue() { return this.value; }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public String getDenominationName() {
+        return this.denominationName;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public static int[] getValidValues() {
+        return VALID_VALUES;
+    }
+
+    //checks if value exists within the denominations
+    private static boolean isValid(int value) {
+        //loop through all valid denominations
+        for (int valid : VALID_VALUES){
+            if (valid == value) {
+                return true;       //returns true if its valid
+            }
+        }
+        return false;              //denomination doesnt exist
+    }
 }
